f80293617b289e034a1751ff0014a0ee5a8c479e
Henry Chan <hchan07@users.noreply.github.com>
2026-03-18 18:24:24 -0400

    added radix-ui, tailwindcss, lucide, clsx, and setup common ui folder and common aliasing (#3)

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+import Input from "@/common/ui/Input";
+
+const Login = () => {
+	return (
+		<Input placeholder="test placeholder"/>
+	)
+};
+
+export default Login;
--- src/routes/_auth/login.tsx
+++ src/routes/_auth/login.tsx
@@ -1,9 +1,6 @@
+import Login from '@/pages/login'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_auth/login')({
-  component: RouteComponent,
+  component: Login,
 })
-
-function RouteComponent() {
-  return <div>Hello "/login"!</div>
-}
