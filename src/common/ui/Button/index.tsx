8f0d41754be7a73f8eb44b0d1ff364f4cf16070d
Henry Chan <hchan07@users.noreply.github.com>
2026-03-31 20:00:01 -0400

    created button component (#5)

---
+++ src/common/ui/Button/index.tsx
@@ -0,0 +1,75 @@
+import cn from "@/common/utils/cn";
+import { cva } from "class-variance-authority";
+
+const buttonVariants = cva([
+		'text-white',
+
+		'rounded-lg',
+    'px-0',
+    'w-full',
+    'font-semibold', 
+    'transition-all',
+    'disabled:opacity-50', 
+    'cursor-pointer', 
+    'py-3',
+
+    // --- Focus Logic ---
+    'focus:outline-2',                // Changed from focus-visible
+    'focus:outline-offset-2',         // Changed from focus-visible
+    'focus:outline-white',        // Changed from focus-visible
+    
+		// --- Hover Logic ---
+    'hover:brightness-125',
+	],
+	{
+		variants: {
+			variant: {
+				primary: 'bg-correct',
+				secondary: 'bg-[#2a2a2b]',
+				ghost: 'bg-transparent hover:bg-[#2a2a2b]',
+				link: 'rounded-none py-0 w-fit inline-flex'
+			},
+			size: {
+				xs: 'text-xs',
+				sm: 'text-sm',
+				default: 'text-base',
+				large: 'text-lg'
+			}
+		},
+		defaultVariants: {
+			variant: 'primary',
+      size: 'default',
+		}
+	},
+);
+
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+	type?: 'button' | 'submit',
+	variant?: 'primary' | 'secondary' | 'ghost' | 'link',
+	size?: 'xs' | 'sm' | 'default' | 'large',
+	isLoading?: boolean,
+	disabled?: boolean
+}
+
+const Button = ({
+	type = 'button',
+	variant,
+	size,
+	children,
+	isLoading,
+	disabled,
+	...props
+} : ButtonProps) => {
+	const className = cn(buttonVariants({variant, size}));
+
+	return (
+		<button 
+			type={type}
+			className={className}
+			disabled={disabled || isLoading}			
+			{...props}
+		>{children}</button>
+	)
+}
+
+export default Button;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,20 @@
+import Button from "@/common/ui/Button";
+import TextInput from "@/common/ui/TextInput";
+
 const Login = () => {
 	return (
-		<div>
-			hello login
+		<div className="m-10 bg-black gap-2 flex flex-col p-2">
+			<Button>
+					hello world
+			</Button>
+			<Button variant="secondary">
+					hello world
+			</Button>			
+			<Button variant="ghost">
+					hello world
+			</Button>			
+			<div className="text-white"><Button variant="link"><span className="text-correct">test link</span></Button> should not wrap</div>
+			<TextInput value="test" type="text" leftIconName="search" name="search" color="green" rightIconName="eye" onRightIconClick={() => console.log('test')} rightAriaLabel="test"/>
 		</div>
 	)
 };
