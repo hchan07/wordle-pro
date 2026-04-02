cfc3da20f0eddc5301c4ecca21c1b767e94ebde9
Henry Chan <henry.chan.tii@gmail.com>
2026-04-02 14:37:36 -0400

    created card component

---
+++ src/common/ui/Card/index.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+
+const Card = ({
+	children
+}: {
+	children: ReactNode
+}) => {
+	return (
+		<div className="rounded-xl border border-gray-500 p-8">
+			{children}
+		</div>
+	)
+};
+
+export default Card;
